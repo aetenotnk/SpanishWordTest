62fbc60f4d21bda46263bb668b9acd3ad467fc4a
Yuta Yamazaki <aetenotnk@ezweb.ne.jp>
2018-12-09 16:14:58 +0900

    DropBoxからファイルをダウンロードするAPI実装

---
+++ app/src/main/java/jp/yutayamazaki/spanishwordtest/dropbox/DropBox.java
@@ -0,0 +1,54 @@
+package jp.yutayamazaki.spanishwordtest.dropbox;
+
+import android.util.Log;
+
+import com.dropbox.core.DbxDownloader;
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class DropBox {
+    private DbxClientV2 client;
+
+    public DropBox(String accessToken, String userAgent){
+        DbxRequestConfig config = DbxRequestConfig.newBuilder(userAgent).build();
+        this.client = new DbxClientV2(config, accessToken);
+    }
+
+    /**
+     * オンライン上からファイルをダウンロードする
+     * @param saveDir 保存するディレクトリ
+     * @param filename ドロップボックス上のアプリケーションフォルダより後のファイル名またはパス
+     * @return String ファイルをダウンロードしたパス
+     */
+    public String downloadFile(String filename, String saveDir){
+        String fromPath = "/" + filename;
+        String toPath = saveDir + "/" + filename;
+
+        try{
+            DbxDownloader downloader  = client.files().download(fromPath);
+            File outFile = new File(toPath);
+
+            InputStreamReader isr = new InputStreamReader(downloader.getInputStream());
+            OutputStreamWriter osr = new OutputStreamWriter(new FileOutputStream(outFile));
+
+            int c;
+            while((c = isr.read()) != -1){
+                osr.write(c);
+            }
+            osr.flush();
+        }
+        catch(DbxException | IOException e){
+            Log.e("DropBoxError", "Cannot downloadTextFile");
+            e.printStackTrace();
+        }
+
+        return toPath;
+    }
+}
