c3003f02407bd839047723060de2ef5d6e701c9f
Yuta Yamazaki <aetenotnk@ezweb.ne.jp>
2018-12-16 14:03:19 +0900

    試験画面の初期化を実装

--- app/src/main/java/jp/yutayamazaki/spanishwordtest/WordTest.java
+++ app/src/main/java/jp/yutayamazaki/spanishwordtest/WordTest.java
@@ -1,20 +1,75 @@
 package jp.yutayamazaki.spanishwordtest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
 
+import jp.yutayamazaki.spanishwordtest.bean.Word;
 import jp.yutayamazaki.spanishwordtest.manager.WordTestManager;
 
 public class WordTest extends AppCompatActivity {
+    // TODO: 2018/12/16 問題数は設定から変更できるようにする
+    private static int TEST_COUNT = 20;
+
     private WordTestManager testManager;
+
+    private TextView questionTextView;
+    private TextView spanishTextView;
+    private TextView japaneseTextView;
+    private Button previousButton;
+    private Button nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_test);
 
-        testManager =WordTestManager.class.cast(
+        testManager = WordTestManager.class.cast(
                 getIntent().getSerializableExtra(TestList.EXTRA_WORD_TEST_MANAGER));
         setTitle(testManager.getTitle());
+
+        questionTextView = findViewById(R.id.question_text);
+        spanishTextView = findViewById(R.id.spanish_text);
+        japaneseTextView = findViewById(R.id.japanese_text);
+        previousButton = findViewById(R.id.previous_button);
+        nextButton = findViewById(R.id.next_button);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        testManager.init(TEST_COUNT);
+        setContents();
+    }
+
+    private void setContents(){
+        Word currentWord = testManager.getCurrentWord();
+        String questionText =
+                this.getResources().getString(R.string.test_question_prefix) +
+                        (testManager.getCurrentTestCount() + 1) +
+                        this.getResources().getString(R.string.test_question);
+
+        // テストの問題文を設定
+        questionTextView.setText(questionText);
+        spanishTextView.setText(WordTestManager.getBlindSpanishExample(currentWord, 0));
+        japaneseTextView.setText(WordTestManager.getJapaneseExample(currentWord, 0));
+
+        // 前へボタンの制御
+        if(testManager.isFirst()){
+            previousButton.setVisibility(Button.INVISIBLE);
+        }
+        else{
+            previousButton.setVisibility(Button.VISIBLE);
+        }
+
+        // 次へボタンのテキストの設定
+        if(testManager.isLast()){
+            nextButton.setText(R.string.test_finish_button);
+        }
+        else{
+            nextButton.setText(R.string.test_next_button);
+        }
     }
 }
--- app/src/main/java/jp/yutayamazaki/spanishwordtest/manager/WordTestManager.java
+++ app/src/main/java/jp/yutayamazaki/spanishwordtest/manager/WordTestManager.java
@@ -1,22 +1,105 @@
 package jp.yutayamazaki.spanishwordtest.manager;
 
 import java.io.Serializable;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import jp.yutayamazaki.spanishwordtest.bean.TestTitle;
 import jp.yutayamazaki.spanishwordtest.bean.Word;
 import jp.yutayamazaki.spanishwordtest.bean.WordCollection;
 
 public class WordTestManager implements Serializable {
+    private static String BLANK = "(    )";
+    private static String EXAMPLE_SPLIT = " / ";
+
     private TestTitle testTitle;
     private List<Word> words;
+    private List<Word> questionWords;
+    private int testCount;
+    private int currentTestCount;
+    private Random random;
 
     public WordTestManager(TestTitle testTitle, WordCollection wordCollection){
         this.testTitle = testTitle;
         this.words = wordCollection.selectAll();
+        this.random = new Random();
+        this.questionWords = new LinkedList<>();
+    }
+
+    public void init(int testCount){
+        this.testCount = testCount;
+        this.currentTestCount = 0;
+
+        setWords();
     }
 
     public String getTitle(){
         return testTitle.getTitle();
+    }
+
+    public Word getCurrentWord(){
+        return questionWords.get(currentTestCount);
+    }
+
+    public void next(){
+        currentTestCount++;
+    }
+
+    public void previous(){
+        currentTestCount--;
+    }
+
+    public boolean isFirst(){
+        return currentTestCount == 0;
+    }
+
+    public boolean isLast(){
+        return currentTestCount == (questionWords.size() - 1);
+    }
+
+    public int getCurrentTestCount(){
+        return currentTestCount;
+    }
+
+    /**
+     * かっこ内を隠したスペイン語の例文を取得する
+     * @param word 単語
+     * @return かっこ内を隠したスペイン語の例文
+     */
+    public static String getBlindSpanishExample(Word word, int index){
+        Pattern pattern = Pattern.compile("\\(.*\\)");
+        Matcher matcher = pattern.matcher(getSpanishExample(word, index));
+
+        return matcher.replaceAll(BLANK);
+    }
+
+    public static String getSpanishExample(Word word, int index){
+        return word.getExampleSpanish().split(EXAMPLE_SPLIT)[index];
+    }
+
+    public static String getJapaneseExample(Word word, int index){
+        return word.getExampleJapanese().split(EXAMPLE_SPLIT)[index];
+    }
+
+    /**
+     * 出題する単語を設定する
+     */
+    private void setWords(){
+        // 例文を含む単語だけ抽出
+        List<Word> filteredWords = words.stream().filter(
+                w -> !w.getExampleSpanish().equals("")).collect(Collectors.toList());
+
+        questionWords.clear();
+
+        // 抽出した単語からランダムに出題する問題を設定
+        Collections.shuffle(filteredWords);
+        for(int i = 0;i < testCount;i++){
+            questionWords.add(filteredWords.get(i));
+        }
     }
 }
