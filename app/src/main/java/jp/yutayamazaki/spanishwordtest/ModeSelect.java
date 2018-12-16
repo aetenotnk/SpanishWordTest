8170b09423b1626cd5dcec1e48fca387ca29f837
Yuta Yamazaki <aetenotnk@ezweb.ne.jp>
2018-12-16 11:22:48 +0900

    モード選択画面から試験画面の遷移を実装

--- app/src/main/java/jp/yutayamazaki/spanishwordtest/ModeSelect.java
+++ app/src/main/java/jp/yutayamazaki/spanishwordtest/ModeSelect.java
@@ -1,20 +1,37 @@
 package jp.yutayamazaki.spanishwordtest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import jp.yutayamazaki.spanishwordtest.manager.WordTestManager;
 
 public class ModeSelect extends AppCompatActivity {
+    private WordTestManager testManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mode_select);
 
-        WordTestManager manager =WordTestManager.class.cast(
+        testManager = WordTestManager.class.cast(
                 getIntent().getSerializableExtra(TestList.EXTRA_WORD_TEST_MANAGER));
-        setTitle(manager.getTitle());
+        setTitle(testManager.getTitle());
+
+        Button startButton = findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent wordTestIntent = new Intent(getApplication(), WordTest.class);
+
+                wordTestIntent.putExtra(TestList.EXTRA_WORD_TEST_MANAGER, testManager);
+
+                startActivity(wordTestIntent);
+                overridePendingTransition(R.anim.in_right, R.anim.out_left);
+            }
+        });
     }
 
     @Override
