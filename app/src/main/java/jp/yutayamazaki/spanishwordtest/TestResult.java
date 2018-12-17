e4d4cbda80e8667447bb2953709f0b501a47a105
Yuta Yamazaki <aetenotnk@ezweb.ne.jp>
2018-12-17 21:13:56 +0900

    テスト結果一覧のウィジェットをRecyclerViewに変更

--- app/src/main/java/jp/yutayamazaki/spanishwordtest/TestResult.java
+++ app/src/main/java/jp/yutayamazaki/spanishwordtest/TestResult.java
@@ -2,11 +2,24 @@ package jp.yutayamazaki.spanishwordtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import jp.yutayamazaki.spanishwordtest.manager.WordTestManager;
 
 public class TestResult extends AppCompatActivity {
     private WordTestManager testManager;
+
+    private TextView scoreText;
+    private TextView baseScoreText;
+    private Spinner filterSpinner;
+    private RecyclerView resultList;
+    private Button tryAgainButton;
+    private Button checkQuestionButton;
+    private Button returnTestListButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +28,13 @@ public class TestResult extends AppCompatActivity {
 
         this.testManager = WordTestManager.class.cast(
                 getIntent().getSerializableExtra(TestList.EXTRA_WORD_TEST_MANAGER));
+
+        this.scoreText = findViewById(R.id.score_actual);
+        this.baseScoreText = findViewById(R.id.score_base);
+        this.filterSpinner = findViewById(R.id.result_filter);
+        this.resultList = findViewById(R.id.result_list);
+        this.tryAgainButton = findViewById(R.id.try_again_button);
+        this.checkQuestionButton = findViewById(R.id.check_button);
+        this.returnTestListButton = findViewById(R.id.return_test_list_button);
     }
 }
