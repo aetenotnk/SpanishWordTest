7cef4aac16c6d1e9c0931f3ec0f9a4f3aaccb5ef
Yuta Yamazaki <aetenotnk@ezweb.ne.jp>
2018-12-22 20:52:41 +0900

    問題確認画面のタイトルを設定

--- app/src/main/java/jp/yutayamazaki/spanishwordtest/CheckQuestion.java
+++ app/src/main/java/jp/yutayamazaki/spanishwordtest/CheckQuestion.java
@@ -3,11 +3,18 @@ package jp.yutayamazaki.spanishwordtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import jp.yutayamazaki.spanishwordtest.manager.WordTestManager;
+
 public class CheckQuestion extends AppCompatActivity {
+    private WordTestManager testManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_question);
+
+        testManager = WordTestManager.class.cast(
+                getIntent().getSerializableExtra(TestList.EXTRA_WORD_TEST_MANAGER));
+        setTitle(testManager.getTitle());
     }
 }
