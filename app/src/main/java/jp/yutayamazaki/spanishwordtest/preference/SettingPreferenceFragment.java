74658e25213399c1ca823069b16a61f25d445d04
Yuta Yamazaki <aetenotnk@ezweb.ne.jp>
2018-12-26 23:04:42 +0900

    「このアプリについて」のバージョンを動的に設定するよう変更

--- app/src/main/java/jp/yutayamazaki/spanishwordtest/preference/SettingPreferenceFragment.java
+++ app/src/main/java/jp/yutayamazaki/spanishwordtest/preference/SettingPreferenceFragment.java
@@ -1,10 +1,14 @@
 package jp.yutayamazaki.spanishwordtest.preference;
 
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 
 import jp.yutayamazaki.spanishwordtest.R;
 
@@ -21,7 +25,9 @@ public class SettingPreferenceFragment extends PreferenceFragment
                 new NumberValidator(TEST_COUNT_MIN, TEST_COUNT_MAX));
         findPreference("about_app").setOnPreferenceClickListener(preference -> {
             String title = getResources().getString(R.string.about_app_title);
-            String message = getResources().getString(R.string.about_app_message);
+            String message_format = getResources().getString(R.string.about_app_message);
+            String versionName = getVersionName();
+            String message = String.format(message_format, versionName);
 
             new AlertDialog.Builder(getActivity())
                     .setTitle(title)
@@ -68,5 +74,26 @@ public class SettingPreferenceFragment extends PreferenceFragment
         String text = sharedPreferences.getString("test_count", "");
 
         findPreference("test_count").setSummary(text);
+    }
+
+    /**
+     * アプリのバージョン名を取得
+     * @return アプリのバージョン名
+     */
+    private String getVersionName(){
+        Context context = getContext();
+
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            PackageInfo info = packageManager.getPackageInfo(context.getPackageName(), 0);
+            Log.d("SpanishWordTest", info.versionName);
+
+            return info.versionName;
+        }
+        catch (PackageManager.NameNotFoundException e){
+            e.printStackTrace();
+        }
+
+        return "";
     }
 }
