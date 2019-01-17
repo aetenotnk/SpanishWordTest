c1c929e7771b65fd9f95eb439dce413e3269bd6d
Yuta Yamazaki <aetenotnk@ezweb.ne.jp>
2019-01-17 19:14:16 +0900

    複数の例文を持つ単語の問題がすべての例文を出題するかのテストを追加

--- app/src/test/java/jp/yutayamazaki/spanishwordtest/QuestionTest.java
+++ app/src/test/java/jp/yutayamazaki/spanishwordtest/QuestionTest.java
@@ -4,6 +4,9 @@ package jp.yutayamazaki.spanishwordtest;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import jp.yutayamazaki.spanishwordtest.bean.Word;
 import jp.yutayamazaki.spanishwordtest.bean.WordType;
 import jp.yutayamazaki.spanishwordtest.manager.Question;
@@ -59,5 +62,58 @@ public class QuestionTest {
 
         Assert.assertEquals(WordTestManager.Grade.NOT_OK,
                 question.evaluateAnswer("abc"));
+    }
+
+    /**
+     * 2つの例文を持つ単語データで2つとも問題として設定されるかテスト
+     */
+    @Test
+    public void randomTowSpanishExamples() {
+        final int LOOP_COUNT = 100;
+
+        Word word = new Word(
+                "Spanish Word",
+                "Japanese Word",
+                "Example1 / Example2",
+                "例文1 / 例文2",
+                new WordType("v", "動詞")
+        );
+        List<String> examples = new LinkedList<>();
+
+        for(int i = 0;i < LOOP_COUNT;i++) {
+            Question question = new Question(word, i);
+
+            examples.add(question.getSpanishExample());
+        }
+
+        Assert.assertTrue(examples.contains("Example1"));
+        Assert.assertTrue(examples.contains("Example2"));
+    }
+
+    /**
+     * 3つの例文を持つ単語データで2つとも問題として設定されるかテスト
+     */
+    @Test
+    public void randomThreeSpanishExamples() {
+        final int LOOP_COUNT = 150;
+
+        Word word = new Word(
+                "Spanish Word",
+                "Japanese Word",
+                "Example1 / Example2 / Example3",
+                "例文1 / 例文2 / 例文3",
+                new WordType("v", "動詞")
+        );
+        List<String> examples = new LinkedList<>();
+
+        for(int i = 0;i < LOOP_COUNT;i++) {
+            Question question = new Question(word, i);
+
+            examples.add(question.getSpanishExample());
+        }
+
+        Assert.assertTrue(examples.contains("Example1"));
+        Assert.assertTrue(examples.contains("Example2"));
+        Assert.assertTrue(examples.contains("Example3"));
     }
 }
